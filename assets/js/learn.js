c414b7743ddd12a6ada5050e8a931c2b1956fa04
sridharan1122@outlook.com <sridharan1122@outlook.com>
2022-10-30 19:19:47 +0530

    17th

--- assets/js/learn.js
+++ assets/js/learn.js
@@ -1,4 +1,4 @@
-
+/* 
 function createViaDom(msg){
     
 const main = document.getElementById("main");
@@ -15,3 +15,12 @@ console.log(div,text);
 }
 createViaDom("FOOD I LOVE ❤");
 createViaDom("FOOD LOVE ❤");
+ */
+
+ const contentEl = document.querySelector(".alert .alert-message");
+// console.log(contentEl.innerText);
+// console.log(contentEl.innerHTML);
+// console.log(contentEl.textContent);
+
+// contentEl.innerText = 'HI sri Garu';
+// contentEl.innerHTML = "<strong> ✔ Successful</strong><i> HI sri Garu</i>"
